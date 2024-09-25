93ca5f8f0bb0a4424c410bb4bca88775e22393fd
emmanuelmejia1516 <a.23308051281197@cbtis128.edu.mx>
2024-09-24 19:46:07 -0600

    Add files via upload

---
+++ farenheit a celsius.py
@@ -0,0 +1,10 @@
+# Solicitar la temperatura en Fahrenheit
+fahrenheit = float(input("Ingresa la temperatura en grados Fahrenheit: "))
+# Convertir a Celsius
+celsius = (5/9) * (fahrenheit - 32)
+# Mostrar el resultado en grados Celsius
+print(f"La temperatura en grados Celsius es: {celsius:.2f}")
+
+print("")
+print("Mejia Suarez Emmanuel Alexander:mi practica es un programa que solicite al usuario el ingreso de una temperatura en escala Fahrenheit y le muestre el equivalente en grados Celsius")
+print("")
